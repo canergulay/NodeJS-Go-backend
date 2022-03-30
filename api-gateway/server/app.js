4df28046356bf4b34b23135991e4e2d1f49904db
canergulay <caner.gulay@hotmail.com>
2022-03-30 03:03:36 +0300

    add: authentication protobuff

--- api-gateway/server/app.js
+++ api-gateway/server/app.js
@@ -7,6 +7,7 @@ const express = require('express')
 const app = express()
 
 app.use('/assets', express.static("assets"));
+app.use('/avatars', express.static("assets/avatars"));
 
 app.use(express.json());
 app.use(express.raw());
